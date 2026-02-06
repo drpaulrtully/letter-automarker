77291cce7b449c3f9226ca458b7d0beabceb9394
drpaulrtully <paulrtully@gmail.com>
2026-02-06 18:42:13 +0000

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -553,3 +553,6 @@ if (modelLetterBtn && modelLetterPanel) {
       showGate("Please enter the access code from your Payhip lesson.");
     });
 })();
+document.getElementById("modelLetterBtn").addEventListener("click", () => {
+  document.getElementById("modelLetterWrap").classList.toggle("open");
+});
